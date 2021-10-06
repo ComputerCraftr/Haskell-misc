6e7af50d1fbad8f33019bea19a0ae69b0b257cd1
John Studnicka <ComputerCraftr@users.noreply.github.com>
2021-10-06 06:59:07 -0700

    Use Karatsuba with base 256

--- src/Karatsuba.hs
+++ src/Karatsuba.hs
@@ -1,14 +1,25 @@
 module Karatsuba
     ( karatsubaMultiplyRecursive
+    , integerDigitsBase256
     ) where
 
-karatsubaMultiplyRecursive :: (Integral a, Show a) => a -> a -> a
-karatsubaMultiplyRecursive multiplicand multiplier
-    | multiplicand < 10 || multiplier < 10 = multiplicand * multiplier
-    | otherwise = do
-        let m2 = min (length (show multiplicand)) (length (show multiplier)) `div` 2
+integerDigitsBase256Helper :: (Integral a) => a -> a -> a
+integerDigitsBase256Helper number digits
+    | number <= 0 = digits
+    -- Divide by 256 until the number is rounded down to zero
+    | otherwise = integerDigitsBase256Helper (number `div` 256) (digits + 1)
 
-        let m2_digit_shift = 10^m2
+integerDigitsBase256 :: (Integral a) => a -> a
+integerDigitsBase256 number = integerDigitsBase256Helper number 0
+
+-- The Karatsuba multiplication algorithm can work with numbers in any base so long as they have more than one digit
+karatsubaMultiplyRecursive :: (Integral a) => a -> a -> a
+karatsubaMultiplyRecursive multiplicand multiplier
+    | multiplicand < 256 || multiplier < 256 = multiplicand * multiplier
+    | otherwise = do
+        let m2 = min (integerDigitsBase256 multiplicand) (integerDigitsBase256 multiplier) `div` 2
+
+        let m2_digit_shift = 256 ^ m2
         let m_digit_shift = m2_digit_shift * m2_digit_shift
 
         let high1 = multiplicand `div` m2_digit_shift
