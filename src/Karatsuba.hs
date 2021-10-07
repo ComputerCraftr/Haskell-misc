502ccd0a60f770b5ed47728c4ca6a9e69ea06947
John Studnicka <ComputerCraftr@users.noreply.github.com>
2021-10-07 03:24:01 -0700

    Switch to quot and rem

--- src/Karatsuba.hs
+++ src/Karatsuba.hs
@@ -7,7 +7,7 @@ integerDigitsBase256Helper :: (Integral a) => a -> a -> a
 integerDigitsBase256Helper number digits
     | number <= 0 = digits
     -- Divide by 256 until the number is rounded down to zero
-    | otherwise = integerDigitsBase256Helper (number `div` 256) (digits + 1)
+    | otherwise = integerDigitsBase256Helper (number `quot` 256) (digits + 1)
 
 integerDigitsBase256 :: (Integral a) => a -> a
 integerDigitsBase256 number = integerDigitsBase256Helper number 0
@@ -17,15 +17,15 @@ karatsubaMultiplyRecursive :: (Integral a) => a -> a -> a
 karatsubaMultiplyRecursive multiplicand multiplier
     | multiplicand < 256 || multiplier < 256 = multiplicand * multiplier
     | otherwise = do
-        let m2 = min (integerDigitsBase256 multiplicand) (integerDigitsBase256 multiplier) `div` 2
+        let m2 = min (integerDigitsBase256 multiplicand) (integerDigitsBase256 multiplier) `quot` 2
 
         let m2_digit_shift = 256 ^ m2
         let m_digit_shift = m2_digit_shift * m2_digit_shift
 
-        let high1 = multiplicand `div` m2_digit_shift
-        let low1 = multiplicand `mod` m2_digit_shift
-        let high2 = multiplier `div` m2_digit_shift
-        let low2 = multiplier `mod` m2_digit_shift
+        let high1 = multiplicand `quot` m2_digit_shift
+        let low1 = multiplicand `rem` m2_digit_shift
+        let high2 = multiplier `quot` m2_digit_shift
+        let low2 = multiplier `rem` m2_digit_shift
 
         let z0 = karatsubaMultiplyRecursive low1 low2
         let z1 = karatsubaMultiplyRecursive (low1 + high1) (low2 + high2)
